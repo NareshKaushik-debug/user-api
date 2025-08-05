6a72cffc9e2824c5ee6fdd9d579d67aecc573b3e
Naresh Kaushik <naresh01@nagarro.com>
2025-08-05 18:09:48 +0530

    adding logs

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,12 +4,15 @@ const User = require('../models/User');
 
 // GET all users
 router.get('/getUsers', async (req, res) => {
+  console.log('Fetching all users');
   const users = await User.find();
+  console.log('Users fetched:', users);
   res.json(users);
 });
 
 // POST a new user
 router.post('/createUser', async (req, res) => {
+  console.log('Creating a new user', req.body);
   const user = new User(req.body);
   await user.save();
   res.status(201).json(user);
